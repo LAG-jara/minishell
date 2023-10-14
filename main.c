862d868fd86d0a6beabee3112302b20d3df55893
Albert Lajara Remon <alajara-@cbr5s1.42barcelona.com>
2023-10-14 13:46:38 +0200

    main_y_tokenize

---
+++ main.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alajara- <alajara-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/14 12:55:53 by alajara-          #+#    #+#             */
+/*   Updated: 2023/10/14 13:45:57 by alajara-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int main(int ac, char **av, char **env)
+{
+	char	*input;
+	char	**tokens;
+	char	***commands;
+	int		exit_status;
+
+	while(42)
+	{
+		input = get_input(env);
+		tokens = tokenize(input);
+		commands = parse(tokens);
+		commands = expand(commands, env);
+		commands = remove_quotes(commands);
+		commands = redirect(commands, env);
+		exit_status = execute(commands, &env);
+		set_exit_status(exit_status, &env);
+	}
+	return (exit_status);
+}
+
+
