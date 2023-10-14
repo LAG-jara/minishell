0ddb40c61fe9279e30fbc19f3617dab387d050fc
Guillem Lajara remon <glajara-@car6s4.42barcelona.com>
2023-10-14 13:51:52 +0200

    NORM: main.c norm-compliance

--- main.c
+++ main.c
@@ -6,18 +6,18 @@
 /*   By: alajara- <alajara-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/14 12:55:53 by alajara-          #+#    #+#             */
-/*   Updated: 2023/10/14 13:45:57 by alajara-         ###   ########.fr       */
+/*   Updated: 2023/10/14 13:50:33 by glajara-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main(int ac, char **av, char **env)
+int	main(int ac, char **av, char **env)
 {
 	char	*input;
 	char	**tokens;
 	char	***commands;
 	int		exit_status;
 
-	while(42)
+	while (42)
 	{
 		input = get_input(env);
 		tokens = tokenize(input);
@@ -30,5 +30,3 @@ int main(int ac, char **av, char **env)
 	}
 	return (exit_status);
 }
-
-
