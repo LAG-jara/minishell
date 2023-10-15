5c853d67c9f826dbb6fb99f48daeeb8d9f178579
Albert Lajara Remon <alajara-@cbr5s3.42barcelona.com>
2023-10-15 18:58:24 +0200

    inUtils_quoteRemoval_added

---
+++ src/input_utils.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   input_utils.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alajara- <alajara-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/15 18:56:50 by alajara-          #+#    #+#             */
+/*   Updated: 2023/10/15 18:56:53 by alajara-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/definitions.h"
+
+// Returns TRUE if c is the char ' ' or '\t', returns FALSE otherwise.
+int	is_blankchr(int c)
+{
+	if(c == ' ' || c == '\t')
+		return (TRUE);
+	return (FALSE);
+}
+
+// Returns TRUE if c is the char '<' or '>', returns FALSE otherwise.
+int is_redirectionchr(int c)
+{
+	if(c == '<' || c == '>')
+		return (TRUE);
+	return (FALSE);
+}
+
+// Returns TRUE if c is the char '<', '>' or '|', returns FALSE otherwise.
+int	is_operatorchr(int c)
+{
+	if(is_redirectionchr(c) || c == '|')
+		return (TRUE);
+	return (FALSE);
+}
+
+// Returns TRUE if c is the char ' ', '\t', '<', '>' or '|', 
+// returns FALSE otherwise.
+int	is_metachr(int c)
+{
+	if(c == ' ' || c == '\t' || is_operatorchr(c))
+		return (TRUE);
+	return (FALSE);
+}
+
+// Returns TRUE if c is the char '\'' or '"', returns FALSE otherwise.
+int is_quotechr(int c)
+{
+	if(c == '\'' || c == '"')
+		return (TRUE);
+	return (FALSE);
+}
---
+++ src/quote_removal.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   quote_removal.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alajara- <alajara-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/15 18:35:10 by alajara-          #+#    #+#             */
+/*   Updated: 2023/10/15 18:35:14 by alajara-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include ""
+
+// Remove pairs of unclosed quotes form left 
+char *remove_quotes_from_token(char *tok)
+{
+	if (is_operatorchr(*tok))
+	while()
+}
+
+// All unquoted occurrences of the characters `'’ and `"’ that did not result
+// from one of the above expansions are removed. 
+char	***remove_quotes(char ***commands)
+{
+	int i_comm;
+	int i_tok;
+
+	i_comm = -1;
+	wihle(commands[++i_comm])
+	{
+		i_tok = -1;
+		while(commands[i_comm][++i_tok])
+			commands[i_comm][i_tok] = \
+			remove_quotes_from_token(commands[i_comm][i_tok]);
+	}
+	return (commands);
+}
