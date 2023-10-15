5cca3e5858f382a3da8757ee1c46335d5d7e0359
Albert Lajara Remon <alajara-@cbr5s3.42barcelona.com>
2023-10-15 19:05:54 +0200

    inUtils.h

---
+++ inc/input_utils.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   input_utils.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alajara- <alajara-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/15 19:02:33 by alajara-          #+#    #+#             */
+/*   Updated: 2023/10/15 19:02:36 by alajara-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef TOKEN_UTILS_H
+# define TOKEN_UTILS_H
+
+
+int	is_blankchr(int c);
+int	is_metachr(int c);
+int	is_operatorchr(int c);
+int	is_redirectionchr(int c);
+int	is_quotechr(int c);
+
+#endif
