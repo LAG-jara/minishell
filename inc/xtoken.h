c08393dc29f21100970f6f333c39fe02fcebef82
Liam-McHara <guillemlr@pm.me>
2023-10-29 13:07:52 +0100

    REFACTOR xtoken funcitons

--- inc/xtoken.h
+++ inc/xtoken.h
@@ -6,7 +6,7 @@
 /*   By: glajara- <glajara-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 13:13:46 by alajara-          #+#    #+#             */
-/*   Updated: 2023/10/29 13:03:41 by glajara-         ###   ########.fr       */
+/*   Updated: 2023/10/29 13:05:48 by glajara-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,8 @@ typedef struct s_xchar
 void	xc_set_flags(int exp_stat, int quote_stat);
 int		xtok_cmp(char *str, t_lst *xstr);
 t_lst	*xtok_rmc(t_lst **xtok, t_lst *to_rm);
-t_lst	*xtok_addc(t_lst **xtok, char c);
-t_lst	*xtok_adds(t_lst **xtok, char *c);
+t_lst	*xtok_addc(t_lst **xtok, char c, int x_flag, int q_flag);
+t_lst	*xtok_adds(t_lst **xtok, char *c, int x_flag, int q_flag);
 char	*xtok_tostr(t_lst **xtok);
 
 #endif
