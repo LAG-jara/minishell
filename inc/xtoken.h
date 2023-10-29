e5d1055ac2f15966663ab583389b924ce15a2c14
Liam-McHara <guillemlr@pm.me>
2023-10-29 15:03:51 +0100

    ADDED t_xtok

--- inc/xtoken.h
+++ inc/xtoken.h
@@ -6,7 +6,7 @@
 /*   By: glajara- <glajara-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 13:13:46 by alajara-          #+#    #+#             */
-/*   Updated: 2023/10/29 13:05:48 by glajara-         ###   ########.fr       */
+/*   Updated: 2023/10/29 15:03:05 by glajara-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,16 @@ typedef struct s_xchar
 	unsigned int	q : 2;
 }	t_xchar;
 
-void	xc_set_flags(int exp_stat, int quote_stat);
-int		xtok_cmp(char *str, t_lst *xstr);
-t_lst	*xtok_rmc(t_lst **xtok, t_lst *to_rm);
-t_lst	*xtok_addc(t_lst **xtok, char c, int x_flag, int q_flag);
-t_lst	*xtok_adds(t_lst **xtok, char *c, int x_flag, int q_flag);
-char	*xtok_tostr(t_lst **xtok);
+typedef t_lst t_xtok;
+
+t_xchar	xc_new(char c, int x_flag, int q_stat);
+t_xtok	*xtok_rm(t_xtok **xtok, t_lst *to_rm);
+t_xtok	*xtok_addc(t_xtok **xtok, char c, int x_flag, int q_flag);
+t_xtok	*xtok_adds(t_xtok **xtok, char *str, int x_flag, int q_flag);
+t_xtok	*xtok_addxc(t_xtok **xtok, t_xchar xc);
+char	*xtok_tostr(t_xtok *xtok);
+
+// DEPRECATED
+int		xtok_cmp(t_xtok *xtok, char *str);
 
 #endif
