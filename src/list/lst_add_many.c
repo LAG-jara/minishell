715c19d7d3f310870252b904f705d2c7944675bd
Liam-McHara <guillemlr@pm.me>
2023-10-31 18:32:29 +0100

    CHANGED lst_add_many adds (after->before) 'pos'

--- src/list/lst_add_many.c
+++ src/list/lst_add_many.c
@@ -6,13 +6,13 @@
 /*   By: glajara- <glajara-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/30 15:55:31 by glajara-          #+#    #+#             */
-/*   Updated: 2023/10/30 16:18:48 by glajara-         ###   ########.fr       */
+/*   Updated: 2023/10/31 18:30:47 by glajara-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "list.h"
 
-// Adds the 'to_add' list nodes right after the 'pos' position.
+// Adds the 'to_add' list nodes right before the 'pos' position.
 void	lst_add_many(t_list **lst, t_list *pos, t_list *to_add)
 {
 	t_list	*prev;
@@ -25,8 +25,8 @@ void	lst_add_many(t_list **lst, t_list *pos, t_list *to_add)
 	}
 	if (!pos)
 		return;
-	prev = pos;
-	next = pos->nxt;
+	prev = pos->pre;
+	next = pos;
 	lst_link(prev, to_add);
 	lst_link(lst_last(to_add), next);
 }
