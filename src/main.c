3135f99dfef94cb9065566d5fd295d4c314b3067
Liam-McHara <guillemlr@pm.me>
2023-11-01 12:08:23 +0100

    rename var environ -> e

--- src/main.c
+++ src/main.c
@@ -6,14 +6,14 @@
 /*   By: glajara- <glajara-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/14 12:55:53 by alajara-          #+#    #+#             */
-/*   Updated: 2023/11/01 11:37:47 by glajara-         ###   ########.fr       */
+/*   Updated: 2023/11/01 12:08:00 by glajara-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 # include "debug.h"
 
-int	main(int ac, char **av, char **environ)
+int	main(int ac, char **av, char **e)
 {
 	char	**env;
 	char	*input;
@@ -22,7 +22,7 @@ int	main(int ac, char **av, char **environ)
 
 	ac++;
 	av[0][0] = '.';
-	env = arrstr_dup(environ);
+	env = arrstr_dup(e);
 	while (42)
 	{
 		input = get_input();
