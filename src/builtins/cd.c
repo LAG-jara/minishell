fd368a610b94e2297743de2321620337a535030f
Albert <wert.lajara@gmail.com>
2023-11-15 18:04:15 +0100

    WIP cd

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -11,7 +11,28 @@
 /* ************************************************************************** */
 
 #include "basic_utils.h"
+#include "env.h"
 #include <unistd.h>
+
+static int	is_relativepath(char *str)
+{
+	if (str && str[0] != '/')
+		return (TRUE);
+	return (FALSE);
+}
+
+// Returns TRUE if can perform a change of dir with any path.
+// FALSE otherwise.
+static int	try_cdpath(char *str, char **path)
+{
+	int	i;
+
+	i = -1;
+	while (path[++i])
+	{
+		if (chdir())
+	}
+}
 
 /*
 cd [dir]
@@ -30,31 +51,42 @@ cd [dir]
 CDPATH
 	The search path for the cd command. This is a colon-separated (`:') list of
 	directories in which the shell looks for destination directories specified
-	by the cd command. A sample value is ".:~:/usr". 
+	by the cd command. A sample value is ".:~:/usr".
 */
 
 int	cd_builtin(char **word, char **env)
 {
-	int err;
-	env += 0;
+	int		i;
+	int		ret;
+
+	i = -1;
 	// Mirar si word[0] es absoluto o relativo ('/' al inicio).
 	// Si es retalivo, buscar en CDPATH word[0]. Si está, escibir el path.
-	err = chdir(*word);
-	if (!err && )
-	return (err);
+	
+	if (is_relativepath(*word))
+	{
+		if (try_cdpath(*word, get_vars("CDPATH" ,env)))
+			return (pwd_builtin());
+	}
+	ret = chdir(*word);
+	if (ret != 0)
+		return (1);
+	return (ret);
 }
 
+int
 
-// # include "debug.h"
-// # include "builtins.h"
-// # include "parse_tokens.h"
-// int	main(int ac, char **av, char **e)
-// {
-// 	char **env = arrstr_dup(e);
-// 	ac += 0;
-// 	av += 0;
-// 	printf("-=-=-=-=-=-=-=-=-=-=--=-=-=-=\n");
-// 	pwd_builtin();
-// 	int err = cd_builtin(++av, env);
-// 	pwd_builtin();
-// }
+
+# include "debug.h"
+# include "builtins.h"
+# include "parse_tokens.h"
+int	main(int ac, char **av, char **e)
+{
+	char **env = arrstr_dup(e);
+	ac += 0;
+	av += 0;
+	printf("-=-=-=-=-=-=-=-=-=-=--=-=-=-=\n");
+	pwd_builtin();
+	int err = cd_builtin(++av, env);
+	pwd_builtin();
+}
