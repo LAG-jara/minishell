0618a50fc827aa956f904b183536b1a155060ede
Liam-McHara <guillemlr@pm.me>
2023-11-16 15:46:33 +0100

    minor comment remove

--- src/redirect_and_execute/redirect_utils.c
+++ src/redirect_and_execute/redirect_utils.c
@@ -6,7 +6,7 @@
 /*   By: glajara- <glajara-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/04 18:25:26 by glajara-          #+#    #+#             */
-/*   Updated: 2023/11/16 14:10:24 by glajara-         ###   ########.fr       */
+/*   Updated: 2023/11/16 15:35:06 by glajara-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,14 @@ int	link_input_file(const char *filename)
 
 	fd_file = open_file(filename, O_RDONLY);
 	if (fd_file == -1)
-		return (EXIT_FAILURE);			// TODO: return appropriate exit code
+		return (EXIT_FAILURE);
 	err = dup2(fd_file, STDIN_FILENO);
 	if (!err)
 		err = close(fd_file);
 	if (err)
 	{
 		print_errno_msg(filename);
-		return (EXIT_FAILURE);			// TODO: return appropriate exit code
+		return (EXIT_FAILURE);
 	}
 	return (0);
 }
@@ -49,14 +49,14 @@ int	link_output_file(const char *filename, int append)
 		flags = O_WRONLY | O_CREAT | O_TRUNC;
 	fd_file = open_file(filename, flags);
 	if (fd_file == -1)
-		return (EXIT_FAILURE);			// TODO: return appropriate exit code
+		return (EXIT_FAILURE);
 	err = dup2(fd_file, STDOUT_FILENO);
 	if (!err)
 		err = close(fd_file);
 	if (err)
 	{
 		print_err_filename(filename);
-		return (EXIT_FAILURE);			// TODO: return appropriate exit code
+		return (EXIT_FAILURE);
 	}
 	return (0);
 }
