9365b3b3f6e7a58f935a41b7347592b38671b514
Liam-McHara <guillemlr@pm.me>
2023-12-16 15:32:22 +0100

    better comment on wait_children

--- src/redirect_and_execute/wait_children.c
+++ src/redirect_and_execute/wait_children.c
@@ -6,15 +6,15 @@
 /*   By: glajara- <glajara-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/27 16:18:17 by glajara-          #+#    #+#             */
-/*   Updated: 2023/12/14 18:04:21 by glajara-         ###   ########.fr       */
+/*   Updated: 2023/12/16 15:31:32 by glajara-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include "basic_utils.h"
 
-// Waits for 'n' children processes to finish and returns the exit status of
-// the 'last_pid' (if exited).
+// Waits for 'n' (n > 0) children processes to finish and returns the 
+// exit status of the 'last_pid' (if exited).
 int	wait_children(pid_t last_pid, size_t n)
 {
 	pid_t	pid;
