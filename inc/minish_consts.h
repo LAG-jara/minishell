be3073f7a14f218bc479f7d9d3daded345651139
Guillem Lajara remon <glajara-@car6s4.42barcelona.com>
2023-12-20 12:20:51 +0100

    ADDED welcome message

--- inc/minish_consts.h
+++ inc/minish_consts.h
@@ -6,7 +6,7 @@
 /*   By: glajara- <glajara-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 12:42:56 by glajara-          #+#    #+#             */
-/*   Updated: 2023/12/18 19:48:33 by glajara-         ###   ########.fr       */
+/*   Updated: 2023/12/20 12:17:55 by glajara-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,9 @@
 
 # define SH_NAME "minish"
 
-# define LINE_1 "\nWelcome to minish - the bare minimum minishell project"
-# define LINE_2 "Our code: https://github.com/lag-jara/minishell"
-# define LINE_3 "Our manual: https://github.com/Liam-McHara/minishell-manual"
+# define LINE_1 "\n\033[33;1m\t ∆\033[32m\tminish"
+# define LINE_2 "\033[33m\t∆ ∆\033[0;2m\t  the bare minimum minishell project\033[0m\n"
+# define LINE_3 "Our code: https://github.com/lag-jara/minishell"
+# define LINE_4 "Our manual: https://github.com/Liam-McHara/minishell-manual"
 
 #endif
--- src/minishell/print_welcome_msg.c
+++ src/minishell/print_welcome_msg.c
@@ -6,13 +6,14 @@
 /*   By: glajara- <glajara-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/04 14:28:03 by glajara-          #+#    #+#             */
-/*   Updated: 2023/12/18 19:48:49 by glajara-         ###   ########.fr       */
+/*   Updated: 2023/12/20 12:14:32 by glajara-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "basic_utils.h"
 #include "minish_consts.h"
 #include <unistd.h>
+#include "print_fx.h"
 
 // Prints the minish welcome message to the standard output.
 void	print_welcome_msg(void)
@@ -20,4 +21,5 @@ void	print_welcome_msg(void)
 	ft_putendl_fd(LINE_1, STDOUT_FILENO);
 	ft_putendl_fd(LINE_2, STDOUT_FILENO);
 	ft_putendl_fd(LINE_3, STDOUT_FILENO);
+	ft_putendl_fd(LINE_4, STDOUT_FILENO);
 }
